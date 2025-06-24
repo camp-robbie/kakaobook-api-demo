de32964885cd3684a49a616778b37aa672233317
camp-robbie <robbie@>
2025-06-25 03:06:49 +0900

    update RestClientConfig

--- src/main/java/com/camp/kakaobookapidemo/config/RestClientConfig.java
+++ src/main/java/com/camp/kakaobookapidemo/config/RestClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestClient;
 
 @Slf4j
@@ -12,14 +13,21 @@ import org.springframework.web.client.RestClient;
 @EnableConfigurationProperties(KakaoProperties.class)
 public class RestClientConfig {
 
-    @Value("${kakao.restapi.key}")
-    private String kakaoKey;
+    private final Environment env;
+
+    public RestClientConfig(Environment env) {
+        this.env = env;
+    }
+
+    public String getKey() {
+        return env.getProperty("kakao.restapi.key");
+    }
 
     @Bean
     public RestClient kakaoRestClient(KakaoProperties props, RestClient.Builder builder) {
-        log.info("[DEBUG] Kakao API Key = {}", kakaoKey);
+        log.info("[DEBUG] Kakao API Key = {}", getKey());
         return builder.baseUrl(props.getApi().getBaseurl() + props.getApi().getPath().getBook())
-                .defaultHeader("Authorization", kakaoKey)
+                .defaultHeader("Authorization", getKey())
                 .build();
     }
 }
